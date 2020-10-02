0c1ebfc4c49d82d111fcc38f6df2c256e4f24c23
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2020-10-02 15:37:16 +0200

    feat: improved solution (224 ms -> 80 ms), docs and test added

--- leetcode/src/1.c
+++ leetcode/src/1.c
@@ -1,17 +1,58 @@
-int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+
+/**
+ * @file
+ * @brief Leetcode problem #1
+ * @details
+ * This is a working solution for the "Two Sum" problem
+ * Runtime: 80ms, memory: N/A, outperforms 82.00% of C submissions.
+ * @author [Gabriele Bruno Franco](https://github.com/gbrunofranco)
+ */
+
+#include "assert.h"
+#include "stdlib.h"
+/**
+ * Function documentation
+ * @param nums array of integers, we iterate on it to find two integers a1, a2
+ * @param numsSize length of nums
+ * @param target we want the result of a1 + a2 to be this integer
+ * @returns 'r' array of the indexes of a1 and a2 in nums
+ * @returns -1 if there's no solution, it is assumed that there's always exactly
+ * one solution
+ */
+int* twoSum(int* nums, int numsSize, int target)
 {
-    int i, j;
-    int *ret = calloc(2, sizeof(int));
-    for (i = 0; i < numsSize; i++)
-    {
-        int key = target - nums[i];
-        for (j = i + 1; j < numsSize; j++)
-            if (nums[j] == key)
+    int* r = malloc(sizeof(int) * 2);
+    for (int i = 0; i < numsSize - 1; ++i)
+        for (int j = i + 1; j < numsSize; ++j)
+            if (nums[i] + nums[j] == target)
             {
-                ret[0] = i;
-                ret[1] = j;
+                r[0] = i;
+                r[1] = j;
+                return r;
             }
-    }
-    *returnSize = 2;
-    return ret;
+    return -1;
+}
+
+/**
+ * @brief Test function
+ * @returns void
+ */
+static void test()
+{
+    /* First leetcode example */
+    assert(twoSum([ 2, 7, 11, 15 ], 4, 9), [ 0, 1 ]);
+    /* Second leetcode example */
+    assert(twoSum([ 3, 2, 4 ], 3, 6), [ 1, 2 ]);
+    /* Third leetcode example */
+    assert(twoSum([ 3, 3 ], 2, 6), [ 0, 1 ]);
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main()
+{
+    test();
+    return 0;
 }
